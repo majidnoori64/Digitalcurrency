2f7236f5bb4ffad58b0cf374c8ff1f3881626db1
majid <noori_majid@yahoo.com>
2023-12-18 01:56:36 +0330

    buttom

--- app.vue
+++ app.vue
@@ -16,6 +16,7 @@
             مرحله بعد
           </button>
           <button
+            v-if="active !== 1"
             @click="active--"
             type="button"
             class="focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
